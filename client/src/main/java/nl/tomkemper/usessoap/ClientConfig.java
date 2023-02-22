b4044d1642b58511937198703f65a22beaae83c4
Tom Kemper <tom.kemper@hu.nl>
2023-02-22 17:53:11 +0100

    HIJ DOET HET

--- client/src/main/java/nl/tomkemper/usessoap/ClientConfig.java
+++ client/src/main/java/nl/tomkemper/usessoap/ClientConfig.java
@@ -1,5 +1,7 @@
 package nl.tomkemper.usessoap;
 
+import com.baeldung.springsoap.gen.GetCountryRequest;
+import com.baeldung.springsoap.gen.GetCountryResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -13,7 +15,7 @@ public class ClientConfig {
 
         //even oppassen, we draaien de service en de client in 1 spring-context, terwijl ze andere serializers nodig hebben:S
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-
+        marshaller.setClassesToBeBound(GetCountryRequest.class, GetCountryResponse.class);
 
         CountryClient client = new CountryClient();
         client.setDefaultUri("http://localhost:8080/ws");
--- client/src/main/java/nl/tomkemper/usessoap/CountryClient.java
+++ client/src/main/java/nl/tomkemper/usessoap/CountryClient.java
@@ -1,15 +1,17 @@
 package nl.tomkemper.usessoap;
 
+import com.baeldung.springsoap.gen.GetCountryRequest;
+import com.baeldung.springsoap.gen.GetCountryResponse;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class CountryClient extends WebServiceGatewaySupport {
-//    public GetCountryResponse getCountry(String country) {
-//        GetCountryRequest request = new GetCountryRequest();
-//        request.setName(country);
-//
-//        GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
-//                .marshalSendAndReceive(request);
-//        return response;
-//    }
+    public GetCountryResponse getCountry(String country) {
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName(country);
+
+        GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
+                .marshalSendAndReceive(request);
+        return response;
+    }
 
 }
