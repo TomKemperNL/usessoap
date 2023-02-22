32baf68591c4d25696c9dbce1e92913731867e0e
Tom <tomkempernl@gmail.com>
2023-02-22 14:35:47 +0100

    Okido, SOAP hello world service werkt...

---
+++ src/main/java/nl/tomkemper/usessoap/CountryEndpoint.java
@@ -0,0 +1,28 @@
+package nl.tomkemper.usessoap;
+
+import com.baeldung.springsoap.gen.Country;
+import com.baeldung.springsoap.gen.GetCountryRequest;
+import com.baeldung.springsoap.gen.GetCountryResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+
+    private static final String NAMESPACE_URI = "http://www.baeldung.com/springsoap/gen";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest req) {
+        String name = req.getName();
+        Country c = new Country();
+        c.setName(name);
+        c.setCapital("Hoofdstad!");
+
+        GetCountryResponse resp = new GetCountryResponse();
+        resp.setCountry(c);
+        return resp;
+    }
+}
