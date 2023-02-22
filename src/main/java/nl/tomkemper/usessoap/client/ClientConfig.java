04b396ea17ea5a7b77259c8f1686171cf70fe705
Tom <tomkempernl@gmail.com>
2023-02-22 16:00:35 +0100

    Dear lord, waarom kan ie m'n classes niet vinden?

--- src/main/java/nl/tomkemper/usessoap/client/ClientConfig.java
+++ src/main/java/nl/tomkemper/usessoap/client/ClientConfig.java
@@ -6,14 +6,13 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
 public class ClientConfig {
+
     @Bean
-    public Jaxb2Marshaller marshaller() {
+    public CountryClient countryClient() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setContextPath("com.baeldung.springsoap.client.gen");
-        return marshaller;
-    }
-    @Bean
-    public CountryClient countryClient(Jaxb2Marshaller marshaller) {
+        marshaller.setPackagesToScan("com.baeldung.springsoap.client.gen");
+
         CountryClient client = new CountryClient();
         client.setDefaultUri("http://localhost:8080/ws");
         client.setMarshaller(marshaller);
--- src/main/java/nl/tomkemper/usessoap/client/CountryClient.java
+++ src/main/java/nl/tomkemper/usessoap/client/CountryClient.java
@@ -1,8 +1,17 @@
 package nl.tomkemper.usessoap.client;
 
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import com.baeldung.springsoap.client.gen.GetCountryRequest;
+import com.baeldung.springsoap.client.gen.GetCountryResponse;
 
 public class CountryClient extends WebServiceGatewaySupport {
+    public GetCountryResponse getCountry(String country) {
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName(country);
 
+        GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
+                .marshalSendAndReceive(request);
+        return response;
+    }
 
 }
